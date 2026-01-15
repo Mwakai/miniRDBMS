19fb8bbccb1f6e2cb541d0a4f197b8c5794bb10d
Mwakai <mwakaimwambala@gmail.com>
2026-01-15 05:26:50 +0300

    init

---
+++ server/Core/Config.php
@@ -0,0 +1,90 @@
+<?php
+
+
+Class Config
+{
+    private static $instance = null;
+
+    private array $config = [];
+
+    private function __construct()
+    {
+        $this->config = [
+            'storage_path' => __DIR__ . '/../Storage',
+            'storage_format' => 'json',
+            'debug_mode' => false,
+            'max_query_time' => 5000,
+            'default_limit' => 1000,
+            'enable_query_log' => false,
+        ];
+    }
+
+    /**
+     * Get singleton instance
+     * 
+     * @return Config
+     */
+    public static function getInstance(): Config
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    
+    /**
+     * Get a configuration value
+     * 
+     * @param string $key Configuration key
+     * @param mixed $default Default value if key doesn't exist
+     * @return mixed
+     */
+    public function get(string $key, $default = null)
+    {
+        return $this->config[$key] ?? $default;
+    }
+    
+    /**
+     * Set a configuration value
+     * 
+     * @param string $key Configuration key
+     * @param mixed $value Configuration value
+     * @return void
+     */
+    public function set(string $key, $value): void
+    {
+        $this->config[$key] = $value;
+    }
+    
+    /**
+     * Check if a configuration key exists
+     * 
+     * @param string $key Configuration key
+     * @return bool
+     */
+    public function has(string $key): bool
+    {
+        return isset($this->config[$key]);
+    }
+    
+    /**
+     * Get all configuration values
+     * 
+     * @return array
+     */
+    public function all(): array
+    {
+        return $this->config;
+    }
+    
+    /**
+     * Reset instance (for testing)
+     * 
+     * @return void
+     */
+    public static function reset(): void
+    {
+        self::$instance = null;
+    }
+    
+}
